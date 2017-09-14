537b3be3414d7f0fd8b6a4608199ccb7fe5d6dbb
hewan <1402365233@qq.com>
2017-09-14 17:12:30 +0800

    讲师管理_注销及启用功能完成

--- views/static/js/teacher/list.js
+++ views/static/js/teacher/list.js
@@ -16,6 +16,7 @@ define(["jquery","template","bootstrap"],function ($,template) {
       }
     })
   
+    
     // 给所有的查看按钮注册点击事件
     $("#teacher_list").on("click",".check-info",function () {
       // 获取当前讲师的id
@@ -39,6 +40,41 @@ define(["jquery","template","bootstrap"],function ($,template) {
         }
       })
     })
+  
+  
+    //讲师的注销以及启用功能分析
+    //讲师账号会有两种状态，已启用 和 已注销
+    //tc_status==0  已启用
+    //tc_status==1  已注销
+  
+    //给所有的注销、启用按钮注册点击事件
+    $("#teacher_list").on("click",".btn-status",function () {
+      var tc_id=$(this).parent().data("id");
+      var tc_status=$(this).data("status");
+      var that=$(this);
+      $.ajax({
+        url:"/api/teacher/handle",
+        type:"post",
+        data:{
+          tc_id:tc_id,
+          tc_status:tc_status
+        },
+        success:function (data) {
+          if(data.code==200){
+            // console.log(data);
+            if(data.result.tc_status==1){
+              that.removeClass("btn-warning").addClass("btn-success").text("启用");
+            }else{
+              that.removeClass("btn-success").addClass("btn-warning ").text("注销");
+            }
+            //需要将当前按钮中保存的 data-status改成修改后的状态
+            that.data("status",data.result.tc_status);
+          }
+        }
+      })
+    })
+    
+    
   
   })
 })
